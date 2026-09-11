b04cb553b8f7be37810ad865164dec301377bf6b
cje361 <edwards.christian494@student.ccm.edu>
2026-09-11 13:26:43 -0400

    completed lab3

--- CelsiusTable.java
+++ CelsiusTable.java
@@ -1,3 +1,13 @@
+
 public class CelsiusTable {
-    
+    public static void main(String[] args) {
+        int Fahrenheit=20;
+        for (int i = 0; i <= 20 ; i++){
+            double Celsius = 0.55556 * (Fahrenheit - 32);
+            System.out.println("Fahrenheit = " + Fahrenheit + "\nCelsius = " + Celsius);
+            Fahrenheit--;
+        }
+            
+        
+    }
 }
--- KineticEnergy.java
+++ KineticEnergy.java
@@ -5,14 +5,9 @@ public class KineticEnergy {
         Scanner input = new Scanner(System.in);
         System.out.println("What is object's mass (in kilograms): ");
         int mass = input.nextInt();
-
         System.out.println("What is object's velocity (in meters per second): ");
         int velocity = input.nextInt();
-
-
         double KineticEnergy= 0.5* mass * Math.pow(velocity,2);
-
         System.out.println("The amount of kinetic energy it has is: " + KineticEnergy);
-
     }
 }
