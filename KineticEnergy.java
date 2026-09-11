20400cd898be6db4982bff6b54e1adb60089d5ce
cje361 <edwards.christian494@student.ccm.edu>
2026-09-11 13:08:34 -0400

    completed lab3

--- KineticEnergy.java
+++ KineticEnergy.java
@@ -1,3 +1,18 @@
-public class KineticEnergy {
+import java.util.Scanner;
 
+public class KineticEnergy {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("What is object's mass (in kilograms): ");
+        int mass = input.nextInt();
+
+        System.out.println("What is object's velocity (in meters per second): ");
+        int velocity = input.nextInt();
+
+
+        double KineticEnergy= 0.5* mass * Math.pow(velocity,2);
+
+        System.out.println("The amount of kinetic energy it has is: " + KineticEnergy);
+
+    }
 }
